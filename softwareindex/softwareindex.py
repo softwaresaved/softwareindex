dd9ce2ef9f853d3ad0bcc805c9d6f122b32384e7
Neil P.Chue Hong <N.ChueHong@software.ac.uk>
2015-03-27 12:08:24 +0000

    Updated softwareindex to illustrate test handler usage

--- softwareindex/softwareindex.py
+++ softwareindex/softwareindex.py
@@ -1,20 +1,47 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, flash
+from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import TextField, SubmitField, validators
+from wtforms.validators import Required
+from handlers import test_handler
 
-app = Flask(__name__)
+class BasicForm(Form):
+    softwarename = TextField('Software Name', description='Software Name.')
+    githubproject = TextField('GitHub Project', description='Github Project.')
+    submit_button = SubmitField('Submit Form') 
 
-@app.route('/')
-def root():
-    return 'Hello World!'
+def create_app(configfile=None):
+    app = Flask(__name__)
+    Bootstrap(app)
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+    app.config['SECRET_KEY'] = 'devkey'
 
-@app.route('/index/test/')
-@app.route('/index/test/<software>')
-def test(software=None):
-    return render_template('test.html', software=software)
+    @app.route('/')
+    def index():
+        form = BasicForm()
+        form.validate_on_submit()
+        flash('critical message', 'critical')
+        flash('error message', 'error')
+        flash('warning message', 'warning')
+        flash('info message', 'info')
+        flash('debug message', 'debug')
+        flash('different message', 'different')
+        flash('uncategorized message')
+        return render_template('index.html', form=form)
+
+    @app.route('/about')
+    def about():
+       return render_template('about.html')
+
+    @app.route('/index/test/')
+    @app.route('/index/test/<software>')
+    def test(software=None, score=-2):
+        handler=test_handler.test_handler()
+        score=handler.get_score(software)
+        description=handler.get_description()
+        return render_template('test.html', software=software, score=score, description=description)
+
+    return app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    create_app().run(debug=True)
